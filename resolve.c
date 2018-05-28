b354df6b8a51188c8571dc5eddbb5c109a9815ad
Eric Scharff <eric.scharff@gmail.com>
2018-05-28 15:54:22 -0400

    StrLn -> PrintLn

--- resolve.c
+++ resolve.c
@@ -1264,7 +1264,7 @@ void resolve_test_file(void) {
   init_global_defs();
   assert(current_scope == &globalScope);
   resolve_scope_push(globalScope.decls);
-  Module *m = parse_test_file("FibFact.ob");
+  Module *m = parse_test_file("Lex.ob");
   resolve_module(m);
   exit_scope("__topdone__");
   assert(current_scope == NULL);
