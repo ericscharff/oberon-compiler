55ddb00ef22e0a27ae792cd868e98da5b4418d02
Eric Scharff <eric.scharff@gmail.com>
2021-10-27 18:52:40 -0600

    Add a new test
    This test computes the knights tour of a chessboard. A C version is
    included as a comparison implementation, and to test correectness.
    
    This produces the correct output with the C code generator, but right
    now seems to hang on the RISC compiler, so the heavy recursion and / or
    code generation may be an issue.

---
+++ tests/KnightsTour.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define N 8
+
+const int move_x[] = {2, 1, -1, -2, -2, -1, 1, 2};
+const int move_y[] = {1, 2, 2, 1, -1, -2, -2, -1};
+int board[N][N];
+
+void setup(void) {
+  for (int i=0; i < N; i++) {
+    for (int j=0; j < N; j++) {
+      board[i][j] = -1;
+    }
+  }
+}
+
+int solve(int curX, int curY, int curMove) {
+  if (curMove == N*N) {
+    return 1;
+  }
+  for (int move=0; move < 8; move++) {
+    int newX = curX + move_x[move];
+    int newY = curY + move_y[move];
+    if ((newX >= 0) && (newX < N) && (newY >= 0) && (newY < N) &&
+        (board[newX][newY] == -1)) {
+      board[newX][newY] = curMove;
+      if (solve(newX, newY, curMove+1)) {
+        return 1;
+      }
+      board[newX][newY] = -1;
+    }
+  }
+  return 0;
+}
+
+void printBoard(void) {
+  for (int i=0; i < N; i++) {
+    for (int j=0; j < N; j++) {
+      printf("%4d", board[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int main(void) {
+  setup();
+  board[0][0] = 0;
+  if (solve(0, 0, 1)) {
+    printf("Success\n");
+  } else {
+    printf("Failure\n");
+  }
+  printBoard();
+  return 0;
+}
