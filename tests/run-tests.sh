c9ab0a59cc4a9d820835ab9c0ad9faaeda9c46b0
Eric Scharff <eric.scharff@gmail.com>
2021-12-09 18:35:04 -0700

    Add tests for Decls
    This runs the tests from Decls.ob. With support for NEW, they run
    properly in the RISC environment as well.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -15,6 +15,7 @@ TESTS=(
   Maze
   Pentominoes
   TestLex
+  TestDecls
   Recurse2
   Recurse
   SetTest
@@ -35,6 +36,7 @@ RISC_TESTS=(
   Maze
   Pentominoes
   TestLex
+  TestDecls
   SetTest
   Shadow
   ShortCircuit
