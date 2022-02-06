870dcc64bc6de05f87298f29f38adb3e671cb7ce
Eric Scharff <eric.scharff@gmail.com>
2022-02-06 15:18:58 -0700

    Simplify cairo code
    One way to use the NATIVE functionality is to assume (as is the case
    with the cairo context) is the first and only element in the
    Cairo_Context object. This avoids the circular delcaration (by simply
    forward declaring the structure) and avoiding the addtiional object
    references.

--- tests/native/cairo.c
+++ tests/native/cairo.c
@@ -5,32 +5,36 @@ typedef struct CairoNative {
   cairo_t *cr;
 } Cairo_Context_Native;
 
-typedef struct Cairo_Context {
-  Cairo_Context_Native _ctx;
-} Cairo_Context;
+typedef struct Cairo_Context Cairo_Context;
 
 void Cairo_InitContext(Cairo_Context *ctx, int width, int height) {
-  ctx->_ctx.surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
-  ctx->_ctx.cr = cairo_create(ctx->_ctx.surface);
+  Cairo_Context_Native *c = (Cairo_Context_Native*)ctx;
+  c->surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
+  c->cr = cairo_create(c->surface);
 }
 
 void Cairo_DestroyContext(Cairo_Context *ctx) {
-  cairo_destroy(ctx->_ctx.cr);
-  cairo_surface_destroy(ctx->_ctx.surface);
+  Cairo_Context_Native *c = (Cairo_Context_Native*)ctx;
+  cairo_destroy(c->cr);
+  cairo_surface_destroy(c->surface);
 }
 
 void Cairo_WriteToPNG(Cairo_Context *ctx, const char *fileName, int len) {
-  cairo_surface_write_to_png(ctx->_ctx.surface, fileName);
+  Cairo_Context_Native *c = (Cairo_Context_Native*)ctx;
+  cairo_surface_write_to_png(c->surface, fileName);
 }
 
 void Cairo_MoveTo(Cairo_Context *ctx, float x, float y) {
-  cairo_move_to(ctx->_ctx.cr, x, y);
+  Cairo_Context_Native *c = (Cairo_Context_Native*)ctx;
+  cairo_move_to(c->cr, x, y);
 }
 
 void Cairo_LineTo(Cairo_Context *ctx, float x, float y) {
-  cairo_line_to(ctx->_ctx.cr, x, y);
+  Cairo_Context_Native *c = (Cairo_Context_Native*)ctx;
+  cairo_line_to(c->cr, x, y);
 }
 
 void Cairo_Stroke(Cairo_Context *ctx) {
-  cairo_stroke(ctx->_ctx.cr);
+  Cairo_Context_Native *c = (Cairo_Context_Native*)ctx;
+  cairo_stroke(c->cr);
 }
