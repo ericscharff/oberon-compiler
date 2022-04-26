66c8e291d992d7a5cb81cc261535166956dbd914
Eric Scharff <eric.scharff@gmail.com>
2022-04-25 19:51:36 -0600

    Add an integer test (exponent with mod)

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -7,6 +7,7 @@ set -e
 TESTS=(
   ArrayTest
   CaseTest
+  ExpMod
   FibFact
   FibFact2
   FibFact3
@@ -15,7 +16,6 @@ TESTS=(
   LangExtensions
   Mandelbrot
   Maze
-  Modulo
   OopTest
   OopTest2
   Pentominoes
