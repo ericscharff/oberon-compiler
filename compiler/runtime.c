b000683dc2b674bbc1997f2d8d42ef725ef63eed
Eric Scharff <eric.scharff@gmail.com>
2024-10-14 20:05:51 -0600

    Update tests to pass with 64-bit integer
    Now all the tests work with the -use_int64 flag (which sets the Oberon
    INTEGER type to be an int64_t in C/C++).

--- compiler/runtime.c
+++ compiler/runtime.c
@@ -201,15 +201,15 @@ void Args_GetArg(char *s, size_t sLen, int arg) {
 }
 
 typedef struct LangExtensions_NatBuf_Native {
-  int nativeQ;
-  int nativeR;
+  OBERON_INTEGER nativeQ;
+  OBERON_INTEGER nativeR;
 } LangExtensions_NatBuf_Native;
 
 typedef struct LangExtensions_NatBuf LangExtensions_NatBuf;
 
-typedef void (*LangExtensions_CallbackFuncNative)(int);
+typedef void (*LangExtensions_CallbackFuncNative)(OBERON_INTEGER);
 
-void LangExtensions_SetNativeVal(LangExtensions_NatBuf *b, int val) {
+void LangExtensions_SetNativeVal(LangExtensions_NatBuf *b, OBERON_INTEGER val) {
   ((LangExtensions_NatBuf_Native*)b)->nativeQ = val;
   ((LangExtensions_NatBuf_Native*)b)->nativeR = val + 1;
 }
@@ -219,7 +219,7 @@ void LangExtensions_CallNative(LangExtensions_NatBuf *b) {
   printf("Native code called, buf.nativeQ = %d, nativeR = %d.\n", nat->nativeQ, nat->nativeR);
 }
 
-void LangExtensions_NativeCallback(int i, LangExtensions_CallbackFuncNative f) {
+void LangExtensions_NativeCallback(OBERON_INTEGER i, LangExtensions_CallbackFuncNative f) {
   printf("About to call callback with i=%d.\n", i+1);
   f(i+1);
   printf("NativeCallback completed\n");
