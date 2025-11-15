4a2640243b91dbce467727683f1b04be30f3c3f4
Eric Scharff <eric.scharff@gmail.com>
2025-11-15 07:26:17 -0700

    Update perr formatting for consistency
    This is part of a larger effort to use automated code tools to format
    code consistently.
    
    This uses the "black" python formatter, which recommends different
    indentation and spacing than older code.

--- compiler/perr.py
+++ compiler/perr.py
@@ -3,14 +3,14 @@
 import fileinput
 
 for line in fileinput.input():
-  line = line.strip()
-  if line.startswith('Err!'):
-    _, file_name, pos, message = line.split('!')
-    pos = int(pos)
-    line_num = 1
-    with open(file_name, 'r') as f:
-      contents = f.read()
-      for i in range(pos):
-        if contents[i] == '\n':
-          line_num += 1
-    print(f'{file_name}:{line_num}:{message}')
+    line = line.strip()
+    if line.startswith("Err!"):
+        _, file_name, pos, message = line.split("!")
+        pos = int(pos)
+        line_num = 1
+        with open(file_name, "r") as f:
+            contents = f.read()
+            for i in range(pos):
+                if contents[i] == "\n":
+                    line_num += 1
+        print(f"{file_name}:{line_num}:{message}")
