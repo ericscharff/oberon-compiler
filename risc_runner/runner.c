3430287f5c7544dd1eab8e847598aecf95c87ef8
Eric Scharff <eric.scharff@gmail.com>
2025-12-09 19:02:35 -0700

    Update register names to correspond with enum

--- risc_runner/runner.c
+++ risc_runner/runner.c
@@ -115,8 +115,10 @@ typedef enum Register {
 } Register;
 
 const char *REG_NAMES[] = {
-    "R0", "R1", "R2",  "R3",  "R4",  "R5", "R6", "R7",
-    "R8", "R9", "R10", "R11", "R12", "GB", "SP", "LR",
+    "R0", "R1", "R2", "R3",
+    "R4", "R5", "R6", "R7",
+    "R8", "R9", "R10", "R11",
+    "R12", "GB", "SP", "LR",
 };
 
 typedef struct Instruction {
