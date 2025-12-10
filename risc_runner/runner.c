182e935a6e9523cf76e1171981e40b5621d4e1f0
Eric Scharff <eric.scharff@gmail.com>
2025-12-09 19:01:32 -0700

    Pack registers
    This was probably introduced by an automatic code format, but this is
    probably more readable.

--- risc_runner/runner.c
+++ risc_runner/runner.c
@@ -108,22 +108,10 @@ const char *INSTR_NAMES[] = {
 };
 
 typedef enum Register {
-  R0,
-  R1,
-  R2,
-  R3,
-  R4,
-  R5,
-  R6,
-  R7,
-  R8,
-  R9,
-  R10,
-  R11,
-  R12,
-  GB,
-  SP,
-  LR,
+  R0, R1, R2, R3,
+  R4, R5, R6, R7,
+  R8, R9, R10, R11,
+  R12, GB, SP, LR,
 } Register;
 
 const char *REG_NAMES[] = {
